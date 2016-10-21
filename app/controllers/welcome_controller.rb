3a37d87ff42b883da864e1281ba6809ffa64f92c
xcello <marisancans@gmail.com>
2016-10-21 09:05:10 +0000

    Session page changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,6 @@ class WelcomeController < ApplicationController
   def home
     @subscribed_feeds = SubscribedFeed.subscribed(current_user)
     @entries = Entry.user_entries(@subscribed_feeds).limit(25).order(published: :desc)
+    @todos = current_user.todos
   end  
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,5 @@
 class Todo < ActiveRecord::Base
+    validates :title, :due_to, presence: true 
+    
+     scope :expired, lambda { where([due_to < ?", Time.current]) }
 end
