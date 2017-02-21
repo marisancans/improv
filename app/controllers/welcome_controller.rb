1de9fd695915d407874886733183ee984807749e
xcello <marisancans@gmail.com>
2017-02-21 20:18:15 +0000

    Fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,6 @@ class WelcomeController < ApplicationController
   def home
     @subscribed_feeds = SubscribedFeed.subscribed(current_user)
     @entries = Entry.user_entries(@subscribed_feeds).limit(25).order(published: :desc)
-    @events = current_user.events.where(start_time: DateTime.current.in_time_zone.beginning_of_day .. DateTime.current.in_time_zone.end_of_month ).order(start_time: :asc)
+    @events_week_in_advance = current_user.events.get_from_week_in_advance
   end  
 end
--- lib/tasks/seeds.rake
+++ lib/tasks/seeds.rake
@@ -4,7 +4,7 @@ namespace :seed do
     10.times do |event|
       name = Faker::Lorem.sentence(3)
       start_time = Faker::Time.between(DateTime.current.beginning_of_month, DateTime.current + 1.month)
-      event = Event.create(name: name, start_time: start_time, user_id: User.first.id)
+      event = Event.create(name: name, start_time: start_time, color: Event::COLORS.sample, user_id: User.first.id)
       event.save
       puts "[#{event.id}] #{start_time} #{name}"
     end
