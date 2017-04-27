0ac7f923d86cceb0782ccff480f30108174024c9
xcello <marisancans@gmail.com>
2017-04-27 08:18:46 +0000

    Rufus timezone change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.time_zone = 'Riga'
+  config.time_zone = 'Europe/Riga'
 
 
 
